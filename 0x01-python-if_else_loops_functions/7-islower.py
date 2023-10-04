8fb076225f11dd5f33bd4ba6273c1c5cbebfdcfb
mariam salama <mariamsalama888@gmail.com>
2023-10-04 12:53:08 -0700

    lower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) in range (97, 123):
+        return True
+    elif ord(c) in range (65, 91):
+        return False
+
+islower = __import__('7-islower').islower
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
+        
