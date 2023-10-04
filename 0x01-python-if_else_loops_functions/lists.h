59cffbdbb45a4ea3ef35430468f33c4903b67790
mariam salama <mariamsalama888@gmail.com>
2023-10-04 14:42:55 -0700

    list

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
