33b12d5d21250b23e03a6d274fe701c2560d34a1
mariam salama <mariamsalama888@gmail.com>
2023-10-08 01:59:47 -0700

    the shadllow copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    elif idx >= 0 and idx < len(my_list)
+    elif idx >= 0 and idx < len(my_list):
        new_list = my_list.copy()
        new_list[idx] = element
        return new_list
