a41e2bb2a31901948a61fbcdaba22b0b1797de1c
mariam salama <mariamsalama888@gmail.com>
2023-11-13 11:41:49 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,17 @@
-#!user/bin/python3
-"""the base class."""
+#!/usr/bin/python3
+
+"""the base class"""
 
 
 class Base:
     """the base class for this project"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """intialization of a new base"""
+        """Initialization of a new Base."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
