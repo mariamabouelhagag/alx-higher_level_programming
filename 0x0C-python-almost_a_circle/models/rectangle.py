55f79a9677432e34df64192a26a15bfd0c8d734d
mariam salama <mariamsalama888@gmail.com>
2023-11-13 16:40:25 -0800

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,9 +7,9 @@ class Rectangle(Base):
     """Represent the rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-    """Initialize a new rectangle"""
-    self.__width = width
-    self.__height = height
-    self.__x = x
-    self.__y = y
-    super().__init__(id)
+     """Initialize a new rectangle"""
+     self.__width = width
+     self.__height = height
+     self.__x = x
+     self.__y = y
+     super().__init__(id)
