66685713a5949af53cb4922a004d11466ed2a469
mariam salama <mariamsalama888@gmail.com>
2024-01-25 14:46:00 -0800

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,9 +2,4 @@
 #Bash script that uses curl to send a GET request to a given URL
 #and then prints the size of the body of the response in bytes.
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 url"
-	exit 1
-
-fi
 curl -s "$1" | wc -c
